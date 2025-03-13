7863194bb4410c0aeb86243b358b1c018a1162bc
cohensh96 <116496490+cohensh96@users.noreply.github.com>
2025-03-13 23:25:59 +0200

    Delete TestedOBCApp/Code/TestedOBCApp directory

--- TestedOBCApp/Code/TestedOBCApp/ExternalLibraries/INIH/tests/runtest.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd "$(dirname "${1}")"
-diff "${1}" <("${2}")
--- TestedOBCApp/Code/TestedOBCApp/TestDataGenerationManager/Sgp4Facade.cpp
+++
@@ -1,7 +0,0 @@
-#include "Sgp4Facade.h"
-
-PointData Sgp4Facade::sgp4(double t) {
-}
-
-PointData* Sgp4Facade::sgp4Array(double t[]) {
-}
